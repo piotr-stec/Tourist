cd73ef97ee03444eec6431abc29946aff1a07d95
ulavrobel <ulawrobel@o2.pl>
2025-01-29 21:23:49 +0100

    main function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
 pub mod db;
 pub mod errors;
 pub mod server;
-fn main() {
-    println!("Hello, world!");
+
+use crate::db::sql_lite::SqliteDb;
+use axum::Server;
+use server::{create_router, AppState};
+use std::net::SocketAddr;
+use std::sync::Arc;
+use tracing::{trace, Level};
+use tracing_subscriber::FmtSubscriber;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+    // init db
+    let db = SqliteDb::new("tourist.db").await?;
+    let state = AppState { db: Arc::new(db) };
+
+    let app = create_router(state);
+    
+    // Start the server
+    let address = SocketAddr::from(([0, 0, 0, 0], 3000));
+    trace!("Listening on {}", address);
+
+    Server::bind(&address)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
 }
